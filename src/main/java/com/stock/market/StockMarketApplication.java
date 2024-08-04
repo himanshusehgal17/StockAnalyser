7b296df472024d4a0e2807323acf9d646743599e
himanshusehgal17 <himanshusehgal17@gmail.com>
2024-08-04 20:57:41 +0530

    Add bootstarp properties

--- src/main/java/com/stock/market/StockMarketApplication.java
+++ src/main/java/com/stock/market/StockMarketApplication.java
@@ -1,14 +1,23 @@
 package com.stock.market;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class StockMarketApplication {
+public class StockMarketApplication implements CommandLineRunner {
+
+	@Value("${market.name}")
+	private String readCloudConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockMarketApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Read Cloud Config : " + readCloudConfig);
+	}
 }
